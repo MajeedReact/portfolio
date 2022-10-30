32ecc28fbabf83b2d8d8c75c3bc0661ea1048a29
MajeedReact <mjalsalhi@gmail.com>
2022-10-30 15:41:39 +0300

    Fuctions were rewritten

--- js/app.js
+++ js/app.js
@@ -1,28 +1,3 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
-
 //getting navbar
 const navBarList = document.querySelector("#navbar__list");
 
@@ -30,54 +5,61 @@ const navBarList = document.querySelector("#navbar__list");
 const lists = ["About Me", "My Stack", "My Porjects", "My Socials"];
 
 //get all sections
-const section = document.querySelectorAll("section");
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+const sections = document.querySelectorAll("section");
 
 // build the nav
-
 function buildNav() {
   //iterate over them and append it to navbar
   for (let i = 0; i < lists.length; i++) {
     const listElement = document.createElement("li");
-    listElement.innerHTML = `<li><a data-nav="section${
+    listElement.innerHTML = `<a navElement="section${
       i + 1
     }" class="menu__link">${lists[i]}</a>`;
-
+    //add event listener to li childeren which is anchorTag
+    listElement.addEventListener("click", () => {
+      //call the function and pass it the child element (anchorTag)
+      navigateToSection(listElement.children[0]);
+    });
     navBarList.appendChild(listElement);
   }
 }
 
-// Add class 'active' to section when near top of viewport
-document.addEventListener("scroll", function (event) {
-  section.forEach((section) => {
+function activeSection(sections) {
+  for (const section of sections) {
+    //get the position of each section
+    const inView = section.getBoundingClientRect();
+
     if (
-      section.getBoundingClientRect().top >= 0 &&
-      section.getBoundingClientRect().top <= 700
+      inView.top >= 0 &&
+      inView.left >= 0 &&
+      inView.bottom <= window.innerHeight &&
+      inView.right <= window.innerWidth
     ) {
       section.classList.add("your-active-class");
+      const currentSection = section.getAttribute("id");
+      //get all li elements and its childs
+      const navElements = navBarList.childNodes;
+      //iterate overthem
+
+      for (const element of navElements) {
+        if (
+          element.childNodes[0].getAttribute("navElement") == currentSection
+        ) {
+          element.childNodes[0].classList.add("element__active");
+        } else {
+          element.childNodes[0].classList.remove("element__active");
+        }
+      }
     } else {
       section.classList.remove("your-active-class");
     }
-  });
-});
-// Scroll to anchor ID using scrollTO event
+  }
+}
 
-/**
- * End Main Functions
- * Begin Events
- *
- */
+//scroll event to the whole document so whenever the document is scrolled the function is called
+document.addEventListener("scroll", () => {
+  activeSection(sections);
+});
 
 //activate hamburger
 const hamburger = document.querySelector(".hamburger");
@@ -89,18 +71,19 @@ hamburger.addEventListener("click", function () {
 // Build menu
 buildNav();
 
-// Scroll to section on link click
-const aboutMe = document.querySelector("li");
-const sections = document.querySelectorAll("section");
+// Scroll to anchor ID using scrollIntoView event
 
-navBarList.addEventListener("click", function (event) {
-  //get element clicked and navigate to its section
-  //accessing data using dataset object
-  if (event.target.dataset.nav) {
-    document.getElementById(`${event.target.dataset.nav}`).scrollIntoView({
+function navigateToSection(element) {
+  //get the attribute value
+  const elementSection = element.getAttribute("navElement");
+
+  //checking whether it is empty or not
+  if (elementSection != null) {
+    //get its corresponding section
+    const section = document.querySelector(`#${elementSection}`);
+    section.scrollIntoView({
       behavior: "smooth",
-      //vertical alignment
       block: "end",
     });
   }
-});
+}
